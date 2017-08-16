bfaeba09343c08225e025be8234ade39e55406dd
gananggww <gananggww@gmail.com>
2017-08-16 15:00:36 +0700

    superior skleton

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+const home = require("./router/home-routing")
+// const login = require("../router/login-routing")
+const karyawan = require("./router/karyawan-routing")
+const user = require("./router/user-routing")
+const absensi = require("./router/absensi-routing")
+const rules = require("./router/rules-routing")
+
+app.use("/", home)
+app.use("/karyawan", karyawan)
+app.use("/absensi", absensi)
+app.use("/rules", rules)
+app.use("/user", user)
+
+
+
+app.listen(3000, ()=> {
+  console.log('Example app listening on port 3000!')
+})
---
+++ router/absensi-routing.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser')
+const db = require("../models")
+
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
+
+
+router.get("/", (req,res)=>{
+  res.send("ini absensi")
+})
+
+module.exports = router
